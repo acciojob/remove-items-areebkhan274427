58cd6c281cb54b9310486ec947ca04527411d6e3
Areeb Khan <55325380+areebkhan274427@users.noreply.github.com>
2023-04-18 20:25:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+const btn = document.querySelector('body > form > input[type=button]');
+
+
+function myFunction(){
+
+	          var x = document.getElementById("colorSelect");
+              x.remove(x.selectedIndex);
+}
+
+btn.addEventListener("click", myFunction);
